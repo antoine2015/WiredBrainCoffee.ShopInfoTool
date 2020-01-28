a989da28d1815a9d6bc6e32dbfdf720c3374ea85
lina akope <lina.akope@saxys.de>
2020-01-28 14:08:22 +0100

    adding wieder

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -18,6 +18,11 @@ namespace WiredBrainCoffee.ShopInfoTool
             {
                 var line = Console.ReadLine();
 
+                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
 
                 if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
