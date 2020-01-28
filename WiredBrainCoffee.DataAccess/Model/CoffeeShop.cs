f5ea7387898faaaee5545b76d060729796745ad9
lina akope <lina.akope@saxys.de>
2020-01-28 13:15:36 +0100

    add data access project to solution

---
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WiredBrainCoffee.DataAccess.Model
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+
+        public int BeansInStockInKg { get; set; }
+    }
+}
