fa7436b4d3c5bb0315c1bc26a7e480593e928df4
lina akope <lina.akope@saxys.de>
2020-01-28 14:10:47 +0100

    local änderung

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -9,8 +9,7 @@ namespace WiredBrainCoffee.ShopInfoTool
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
 
-            Console.WriteLine("Write 'help' to list available coffe shop commands, " +
-              "write 'quit' to exit application");
+            Console.WriteLine("local änderung ");
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
 
