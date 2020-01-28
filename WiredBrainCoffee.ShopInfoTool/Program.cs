075b4c7ea3de915fc850d4aa867bc7e62c117add
lina akope <lina.akope@saxys.de>
2020-01-28 14:07:05 +0100

    delete from

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -18,11 +18,6 @@ namespace WiredBrainCoffee.ShopInfoTool
             {
                 var line = Console.ReadLine();
 
-                if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
-                {
-                    break;
-                }
-
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
 
                 if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
