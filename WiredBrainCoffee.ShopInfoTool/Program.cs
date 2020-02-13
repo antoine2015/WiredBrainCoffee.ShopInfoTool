3db0706bd92bc836fd2d1e399b02736ffa10d85c
Lina Akope <lina.akope@saxsys.de>
2020-02-13 12:58:50 +0100

    Change from B

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -6,6 +6,7 @@ namespace WiredBrainCoffee.ShopInfoTool
 {
     class Program
     {
+        // change made from featureB
         // change made for feature 
         static void Main(string[] args)
         {
